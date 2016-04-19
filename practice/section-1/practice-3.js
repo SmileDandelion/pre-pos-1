42a823bd133a4a64e12a776141bbb614cfca7058
ning <1239359586@qq.com>
2016-04-19 21:38:21 +0800

    practice1.3

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,3 +1,18 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var newArray = [];
+  collection_a.forEach(function (element) {
+    if (isExist(element, object_b)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+}
+function isExist(collection, object_b) {
+  for (var i = 0; i < object_b.value.length; i++) {
+    if (collection === object_b.value[i]) {
+      return true;
+    }
+  }
+  return false;
 }
